a2d59a2e8bf32559f7780247827893df0d3bbdfd
SeokJin <46406119+seokijn0706@users.noreply.github.com>
2020-03-02 07:06:55 +0900

    에라토스테네스의 체 알고리즘

--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -8,7 +8,7 @@ void SieveOfEratosthenes() {
    for (int i = 2; i <= MAX; i++) {
       isPrime[i] = true; // 처음에 모두 소수라고 가정
    }
-   for (int i = 2; i <= MAX; i++) { //2 부터 n 까지
+   for (int i = 2; i * i <= MAX; i++) { //2 부터 sqrt(n) 까지
       if (!isPrime[i]) continue; //이미 지워진 수는 무시
       for (int j = i * i; j <= MAX; j += i) { // 자기자신을 제외한 배수를 모두
          isPrime[j] = false; // 지운다
